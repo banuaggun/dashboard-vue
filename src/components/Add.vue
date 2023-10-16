b2bd99fe5bde7e7acedb150667202431f7bf1155
banuaggun <banuaggun@outlook.com>
2023-10-16 21:23:34 +0300

    Product Add Form is being prepared

--- src/components/Add.vue
+++ src/components/Add.vue
@@ -12,11 +12,29 @@ export default {
   <button @click="open = true" class="add">
     <span>+</span>
   </button>
-
-  <div v-if="open" class="modal">
-    <p>Hello from the modal!</p>
-    <button @click="open = false">Close</button>
+  <div class="modal__area" v-if="open">
+  <div class="modal__area__content">
+    <div class="modal__area__content__close">
+      <button @click="open = false">
+        <i class="ph-thin ph-x-circle"></i>
+      </button>
+    </div>
+    <div class="modal__area__content__form">
+      <form>
+        <label for="name">Name</label>
+        <input type="text" id="name" value="" />
+        <label for="phone">Phone</label>
+        <input type="text" id="phone" value="" />
+        <label for="orders">Orders</label>
+        <input type="text" id="orders" value="" />
+        <label for="spends">Spends</label>
+        <input type="text" id="spends" value="" />
+      </form>
+    </div>
   </div>
+  </div>
+
+
 
   <!-- 
     <button @click="open = true">Open Modal</button>
@@ -45,13 +63,53 @@ export default {
   font-weight:bold;
 }
 
-.modal{
-  position:fixed;
+.modal__area{
+  position:absolute;
+  top:0;
+  left:0;
+  background-color: rgba(0, 0, 0, 25%);
   z-index:999;
+  width:100%;
+  height:100%;
+}
+
+.modal__area__content{
 background-color:white;
   width: 60%;
-  height:calc(100% - 300px);
+  height:calc(100% - 200px);
+  position:relative;
   margin: 75px auto;
   border:1px solid green;
+}
+
+.modal__area__content__close{
+  position:absolute;
+  right:20px;
+  top:20px;
+}
+
+.modal__area__content__close i{
+  font-size:2.5rem;
+}
+
+.modal__area__content__form form{
+  margin:60px 40px 30px 40px;
+  display:flex;
+  flex-direction:column;
+  border:1px solid green;
+}
+
+.modal__area__content__form form label{
+  font-size:1.2rem;
+  padding:8px 0;
+  font-weight:700;
+}
+
+.modal__area__content__form input{
+  width:80%;
+  height:32px;
+  outline:none;
+  padding-left:10px;
+  margin:0 0 20px 0;
 }
 </style>
