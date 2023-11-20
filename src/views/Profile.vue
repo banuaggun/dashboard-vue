5675443619ca29d03d1b03f6da83f27fc4477908
banuaggun <banuaggun@outlook.com>
2023-11-20 22:16:36 +0300

    Profile Page editing has started

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,5 +1,11 @@
 <template>
-  profile page
+  <div class="profile-page">
+    <div class="profile-page-wrapper">
+      <div class="column column-1">1</div>
+      <div class="column column-2">2</div>
+      <div class="column column-3">3</div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -9,5 +15,12 @@ export default {
 </script>
 
 <style>
-
+.profile-page-wrapper{
+  display:grid;
+  grid-template-columns: repeat(auto-fit, 20rem);
+  justify-content:space-between;
+}
+.column{
+  border:1px solid fuchsia;
+}
 </style>
