6945c34684456b232edf5a1a9a0678e8f181f575
banuaggun <banuaggun@outlook.com>
2023-12-18 22:27:41 +0300

    Necessary form editing is being done for the profile page.

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,30 +1,72 @@
 <template>
-  <div class="profile-page">
-    <div class="profile-page-wrapper">
-      <div class="column column-1">
-        <OverviewProfileVue/>
+  <div>
+    <form>
+      <div>
+        <input type="text" id="name" v-model="name" :disabled="!editing" />
       </div>
-      <div class="column column-2">2</div>
-      <div class="column column-3">3</div>
-    </div>
+      <div>
+        <input type="email" id="email" v-model="email" :disabled="!editing" />
+      </div>
+      <div>
+        <input type="tel" id="phone" v-model="phone" :disabled="!editing" />
+      </div>
+      <div>
+        <button :class="{edit: !editing}" @click.prevent="editing = !editing">{{ editing ? 'Save' : 'Edit' }}</button>
+        <button v-if="editing" @click.prevent="cancel()">Cancel</button>
+      </div>
+    </form>
   </div>
 </template>
 
-<script setup>
-import OverviewProfileVue from '../components/OverviewProfile.vue'
-
+<script>
+export default {
+  data() {
+    return {
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      phone: '555-555-5555',
+      editing: false,
+      originalName: '',
+      originalEmail: '',
+      originalPhone: '',
+    };
+  },
+  methods: {
+    cancel() {
+      this.name = this.name;
+      this.email = this.email;
+      this.phone = this.phone;
+      this.editing = false;
+    },
+  },
+  watch: {
+    name(value) {
+      if (!this.originalName) {
+        this.originalName = value;
+      }
+    },
+    email(value) {
+      if (!this.originalEmail) {
+        this.originalEmail = value;
+      }
+    },
+    phone(value) {
+      if (!this.originalPhone) {
+        this.originalPhone = value;
+      }
+    },
+  },
+};
 </script>
-
 <style>
-.profile-page-wrapper{
-  display:grid;
-  grid-template-columns: repeat(auto-fit, 20rem);
-  justify-content:space-evenly;
+input{
+  border:0;
+  background-color:transparent;
+  color:#100;
 }
-.column{
-  border:1px solid fuchsia;
-  height:100vh;
-  padding:20px 0;
+button.edit:focus input{
+  border-bottom: 2px solid #f00;
 }
+
 
 </style>
